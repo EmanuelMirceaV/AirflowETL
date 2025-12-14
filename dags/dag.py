6ff10501cdf8a93bd3d884d3dc9bee7f5ab899af
EmanuelMirceaV <emanuel.mircea.voina@gmail.com>
2025-12-14 22:20:28 +0200

    DAG - commit 1

---
+++ dags/dag.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+headers = {
+    "Referer": 'https://www.amazon.com/',
+    "Sec-Ch-Ua": "Not_A Brand",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "macOS",
+    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+}
