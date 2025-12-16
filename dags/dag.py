393b61dbc39e702f9b700e4c03b876c158ffaf1e
EmanuelMirceaV <emanuel.mircea.voina@gmail.com>
2025-12-16 21:44:15 +0200

    DAG - commit 2

--- dags/dag.py
+++ dags/dag.py
@@ -1,12 +1,32 @@
 from datetime import datetime, timedelta
-from airflow import DAG
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-from airflow.operators.python import PythonOperator
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.sdk import DAG
 
+#import requests
+#import pandas as pd
+#from bs4 import BeautifulSoup
+#from airflow.operators.python import PythonOperator
+#from airflow.providers.postgres import PostgresOperator
+#from airflow.providers.postgres.hooks import PostgresHook
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 12, 15),
+    #'email_on_failure': False,
+    #'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'amazon_books_scraper',
+    default_args=default_args,
+    description='A DAG to scrape Amazon product data and store it in Postgres',
+    schedule=timedelta(days=1), # want it to run daily
+    #catchup=False,
+)
+
+'''
 headers = {
     "Referer": 'https://www.amazon.com/',
     "Sec-Ch-Ua": "Not_A Brand",
@@ -14,3 +34,4 @@ headers = {
     "Sec-Ch-Ua-Platform": "macOS",
     'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 }
+'''
